58031886b46b3eb08aa9612e18141c5e0320f342
jvinkovic <josip.vinkovic.josip@gmail.com>
2023-06-07 21:08:13 +0200

    liste

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ import CounterClassComponent from './CounterClassComponent';
 import CounterFunctionComponent from './CounterFunctionComponent';
 
 function App() {
+
+  const numbers = [1,2,5,4,5];
+  const osobe = [{ime: 'pero', godine: 34}, {ime: 'janko', godine: 22}]
+
   return (
     <div>
       App.js
+      <ul>
+        {numbers.map((elementListe, index) => {
+          return <li key={index}>
+                    <b>{elementListe}</b>
+                </li>;
+        })}
+      </ul>
+
+      <ul>
+        {osobe.map((elementListe, index) => {
+          return <li key={index}>
+                    <b>{elementListe.ime} ima godina: {elementListe.godine}</b>
+                </li>;
+        })}
+      </ul>
 
       <CounterClassComponent />
       <hr/>
